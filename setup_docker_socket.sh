a9ef1108c14fa479ff5f4056f7bf7b0f527a93b1
davtyan111 <106110381+davtyan111@users.noreply.github.com>
2023-07-25 19:12:52 +0400

    Create setup_docker_socket.sh

---
+++ setup_docker_socket.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if the Docker socket exists
+if [ -S "/var/run/docker.sock" ]; then
+    # Change the group ownership of the Docker socket
+    sudo chgrp $(id -g) /var/run/docker.sock
+    # Add write permissions for the group
+    sudo chmod 666 /var/run/docker.sock
+else
+    echo "Docker socket not found."
+    exit 1
+fi
