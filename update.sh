6bdc3b94cd10abfc31d3ef3d55af7267b14d5550
davtyan111 <davtyan.rafayel.37@gmail.com>
2023-09-04 17:48:42 +0400

    modify buildspec.yml add update.sh

--- update.sh
+++ update.sh
@@ -3,20 +3,20 @@
 # Command to perform a rolling restart of pods in a Deployment
 kubectl_command="kubectl rollout restart deployment/laravel-deployment -n laravel"
 
-# AWS access key ID, secret access key, region, and EKS cluster name
-access_key_id=$access_key_id
-secret_access_key=$secret_access_key
-aws_region=$aws_region
-eks_cluster_name=$cluster
+# Retrieve AWS access key ID, secret access key, region, and EKS cluster name from environment variables
+access_key_id="$access_key_id"
+secret_access_key="$secret_access_key"
+aws_region="$AWS_REGION"
+eks_cluster_name="$EKS_CLUSTER_NAME"
 
 # Create an AWS EKS client
 eks_client="aws eks --region $aws_region"
 
 try {
   # Get the EKS cluster configuration
-  cluster_info=$(eks_client describe-cluster --name $eks_cluster_name)
-  cluster_certificate_data=$(echo $cluster_info | jq -r '.cluster.certificateAuthority.data')
-  cluster_endpoint=$(echo $cluster_info | jq -r '.cluster.endpoint')
+  cluster_info=$(eks_client describe-cluster --name "$eks_cluster_name")
+  cluster_certificate_data=$(echo "$cluster_info" | jq -r '.cluster.certificateAuthority.data')
+  cluster_endpoint=$(echo "$cluster_info" | jq -r '.cluster.endpoint')
 
   # Save the cluster configuration to a kubeconfig file
   kubeconfig_file_path="/tmp/kubeconfig.yaml"
@@ -46,10 +46,10 @@ users:
       - $eks_cluster_name
       command: aws
       env: null
-      interactiveMode: "Never"" > $kubeconfig_file_path
+      interactiveMode: "Never"" > "$kubeconfig_file_path"
 
   # Execute the kubectl command using the kubeconfig file
-  $kubectl_command --kubeconfig $kubeconfig_file_path
+  "$kubectl_command" --kubeconfig "$kubeconfig_file_path"
   echo "Command executed successfully."
 } catch {
   e=$(echo $?)
