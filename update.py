5547ced1680079fb01cb20ac71e7f635341ef3a6
davtyan111 <davtyan.rafayel.37@gmail.com>
2023-09-04 12:02:36 +0400

    update.py

--- update.py
+++ update.py
@@ -1,11 +1,7 @@
 import subprocess
 import boto3
 
-# Замените на свои учетные данные AWS
-# aws_access_key = ""
-# aws_secret_key = ""
-region_name = "us-east-2"
-eks_cluster_name = 'clusters'
+
 
 # Команда для выполнения перезапуска подов в Deployment
 kubectl_command = "kubectl rollout restart deployment/laravel-deployment -n laravel"
@@ -13,9 +9,9 @@ kubectl_command = "kubectl rollout restart deployment/laravel-deployment -n lara
 # Создаем клиент EKS
 eks_client = boto3.client(
     'eks',
-    aws_access_key_id=aws_access_key,
-    aws_secret_access_key=aws_secret_key,
-    region_name=region_name
+    aws_access_key_id=access_key_id,
+    aws_secret_access_key=secret_access_key,
+    region_name=aws-region
 )
 
 try:
