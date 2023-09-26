d5ec1478d4e59b84a1ecb54e0708ede480244852
davtyan111 <106110381+davtyan111@users.noreply.github.com>
2023-09-26 14:51:09 +0400

    Update python.py

--- python.py
+++ python.py
@@ -1,10 +1,15 @@
 import requests
 
-def send_slack_notification(message):
+def send_slack_notification(message, status):
     webhook_url = 'https://hooks.slack.com/services/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'
     payload = {
         'text': message
     }
+
+    if status:
+        payload['text'] = f'Успешно: {message}'
+    else:
+        payload['text'] = f'Ошибка: {message}'
 
     response = requests.post(webhook_url, json=payload)
 
@@ -13,5 +18,8 @@ def send_slack_notification(message):
     else:
         print(f'Произошла ошибка {response.status_code} при отправке уведомления в Slack')
 
-# Пример использования:
-send_slack_notification('Ваше уведомление с помощью App Runner')
+# Пример использования при успешном завершении:
+send_slack_notification('Деплоймент завершен успешно', True)
+
+# Пример использования при ошибке:
+send_slack_notification('Произошла ошибка при деплойменте', False)
